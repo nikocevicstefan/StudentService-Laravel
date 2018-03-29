ff67d4124bc59aeb301a57edb7805fe9efa9b8d5
nstefan <nikocevicstefan@gmail.com>
2018-03-29 09:42:30 +0200

    Routes grouped by middleware

--- routes/api.php
+++ routes/api.php
@@ -16,27 +16,51 @@ use Illuminate\Http\Request;
 Route::post('login', 'AuthController@login');
 Route::post('register', 'AuthController@register');
 
-
 Route::get('subjects/{subject}/students', 'SubjectController@showStudents');
 Route::get('subjects/{subject}/professors', 'SubjectController@showProfessors');
 Route::get('students/{student}/subjects', 'StudentController@showSubjects');
 
 Route::group(['middleware'=>['jwt.auth']], function()
 {
+    Route::group(['middleware'=>'App\Http\Middleware\Admin'], function()
+    {
+        Route::resource('roles','RoleController');
+        Route::resource('payments','PaymentController');
+        Route::resource('users','UserController');
 
-    Route::resource('student_subject','Student_SubjectController');
-    Route::resource('subject_professor','Subject_ProfessorController');
-    Route::resource('semesters','SemesterController');
-    Route::resource('courses','CourseController');
-    Route::resource('payments','PaymentController');
-    Route::resource('students','StudentController');
-    Route::resource('professors','ProfessorController');
-    Route::resource('users','UserController');
-    Route::resource('roles','RoleController');
-    Route::resource('subjects','SubjectController');
+        Route::resource('semesters','SemesterController',['except' => 'index','show']);
+        Route::resource('courses','CourseController',['except' => 'index','show']);
+        Route::resource('students','StudentController',['except' => 'index','show']);
+        Route::resource('professors','ProfessorController',['except' => 'index','show']);
+        Route::resource('subjects','SubjectController',['except' => 'index','show']);
+
+        Route::resource('subject_professor','Subject_ProfessorController');
+
+    });
+
+    Route::group(['middleware'=>'App\Http\Middleware\Professor'], function()
+    {
+        Route::resource('student_subject','Student_SubjectController');
+    });
+
+    Route::group(['middleware'=>'App\Http\Middleware\Student'], function()
+    {
+
+    });
+
+    Route::get('semesters','SemesterController@index');
+    Route::get('courses','CourseController@index');
+    Route::get('students','StudentController@index');
+    Route::get('professors','ProfessorController@index');
+    Route::get('subjects','SubjectController@index');
+
+    Route::get('semesters','SemesterController@show');
+    Route::get('courses','CourseController@show');
+    Route::get('students','StudentController@show');
+    Route::get('professors','ProfessorController@show');
+    Route::get('subjects','SubjectController@show');
+
     Route::get('logout', 'AuthController@logout');
 });
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
